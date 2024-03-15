680b27e5cb22fc6e2aa1be74bd00c0a417c71823
crux <seymour.jordan93@gmail.com>
2024-03-15 01:03:41 -0500

    parlaccs

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject background;
+    public float speed = 0.5f;
+
+    private GameObject background2;
+    private float spriteWidth;
+
     void Start()
     {
-        
+        // Duplicate the background sprite
+        background2 = Instantiate(background, new Vector3(background.transform.position.x + background.GetComponent<SpriteRenderer>().bounds.size.x, background.transform.position.y, background.transform.position.z), Quaternion.identity);
+
+        // Get the width of the sprite
+        spriteWidth = background.GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Move the sprites to the left
+        background.transform.Translate(-speed * Time.deltaTime, 0, 0);
+        background2.transform.Translate(-speed * Time.deltaTime, 0, 0);
+
+        // If one sprite is off the screen, move it to the right side of the other sprite
+        if (background.transform.position.x < -spriteWidth)
+        {
+            background.transform.position = new Vector3(background2.transform.position.x + spriteWidth, background.transform.position.y, background.transform.position.z);
+        }
+        if (background2.transform.position.x < -spriteWidth)
+        {
+            background2.transform.position = new Vector3(background.transform.position.x + spriteWidth, background2.transform.position.y, background2.transform.position.z);
+        }
     }
 }
